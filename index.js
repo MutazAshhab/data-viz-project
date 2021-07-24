2325c50f34ff20216b0a6a876bb8a0189eacf3dc
Mutaz <mutaz.ashhab@gmail.com>
2021-07-24 21:30:47 +1000

    Moved index.js file

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 app = express();
 
 PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, "server", "public")));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
