8d0a565cea98ebad4c2299f2a2aca7c40ff0a28c
Mutaz <mutaz.ashhab@gmail.com>
2021-07-25 18:18:03 +1000

    Added server side code

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const path = require("path");
 
 app = express();
 
-PORT = process.env.PORT || 5000;
+app.use(express.json({ limit: "1mb" }));
 
 app.use(express.static(path.join(__dirname, "server", "public")));
 
+app.use("/api/svg", require("./routes/api/svg"));
+
+PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
