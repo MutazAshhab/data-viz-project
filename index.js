5637b1626dedd46fac76e95bdd28d69b72365e90
Mutaz <mutaz.ashhab@gmail.com>
2021-08-04 22:06:18 +1000

    Added API route.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.json({ limit: "1mb" }));
 app.use(express.static(path.join(__dirname, "server", "public")));
 
 app.use("/api/svg", require("./routes/api/svg"));
+app.use("/api/data", require("./routes/api/data"));
 
 PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
