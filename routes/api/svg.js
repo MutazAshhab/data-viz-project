0b0aa7b990e601cafebb53c3b83efa729c7d9e9e
Mutaz <mutaz.ashhab@gmail.com>
2021-08-04 21:58:59 +1000

    Removed comment

--- routes/api/svg.js
+++ routes/api/svg.js
@@ -4,9 +4,7 @@ const getBarChart = require("../../server/src/barchart/barchartgenerator");
 
 // Body of request contains data on the plot to be generated.
 router.post("/", (req, res) => {
-    // TODO: validate req.body
-    // Joi => Validation, JSdom to to create an HTML <body> DOM element on which D3js could hook
-    // getBarChart(req.body);
+    // TODO: validate req.body using Joi
     getBarChart(req.body, (outerHTHML) => {
         res.json({ chart: outerHTHML });
     });
