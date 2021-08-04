f93b1c430ed259cf1b6d5f11b7e429080d3a9d4d
Mutaz <mutaz.ashhab@gmail.com>
2021-08-04 22:08:49 +1000

    Removed hardcoded data

--- server/src/barchart/barchartgenerator.js
+++ server/src/barchart/barchartgenerator.js
@@ -4,18 +4,11 @@ const barChart = require("./barchart");
 
 let d3 = d3Promise.then((lib) => (d3 = lib));
 
-// HARDCODED: Remove data import
-// TODO remove hardcoded data.
-const data2 = require("../../data/data2.json");
-
 const { JSDOM } = jsdom;
 const document = new JSDOM().window.document;
 
-// NOTE: Use JSdom to create an HTML <body> DOM element on which D3js could hook
 function getBarChart(params, callback) {
     // HARDCODED: Remove addition of params.
-    // TODO remove hardcoded data2.
-    params["data"] = data2;
     params["xVar"] = "region";
     params["yVar"] = "value";
     params["xAxisLabel"] = "x-label";
