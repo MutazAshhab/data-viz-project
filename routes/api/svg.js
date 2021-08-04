53c50b3fe978adec652eaf4ce7371c441132e5f0
Mutaz <mutaz.ashhab@gmail.com>
2021-08-04 21:01:46 +1000

    Updated API response

--- routes/api/svg.js
+++ routes/api/svg.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const getBarChart = require("../../server/src/barchart/barchartgenerator");
 
 // Body of request contains data on the plot to be generated.
 router.post("/", (req, res) => {
-    res.json({ response: "working!" });
+    // TODO: validate req.body
+    // Joi => Validation, JSdom to to create an HTML <body> DOM element on which D3js could hook
+    // getBarChart(req.body);
+    getBarChart(req.body, (outerHTHML) => {
+        res.json({ chart: outerHTHML });
+    });
 });
 
 module.exports = router;
